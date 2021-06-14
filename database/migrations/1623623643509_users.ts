33dc914eec9ce76f7177eec479c7971118a8e4bc
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-06-13 21:15:42 -0300

    First Migration 💯

---
+++ database/migrations/1623623643509_users.ts
@@ -0,0 +1,23 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Users extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('email').unique().notNullable()
+      table.string('name')
+      table.string('username').notNullable()
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
