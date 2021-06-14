758e17e05d3494b2785979fec26a7fdbd0dfeb06
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-06-14 11:37:20 -0300

    Added Contrpller of User Show 💯

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -18,18 +18,37 @@ export default class UsersController {
     return response.status(201).send(user)
   }
 
+  public async show({ request, response }: HttpContextContract) {
+    const { id } = request.params()
+
+    if (!Number(id)) return response.status(400).send({ message: 'ID Not number' })
+
+    try {
+      const user = await User.findOrFail(id)
+      return response.status(200).json(user)
+    } catch (error) {
+      return response.status(400).json({ message: 'User already exists' })
+    }
+  }
+
   public async index({ response }: HttpContextContract) {
-    const users = await User.all()
-    return response.status(200).send(users)
+    try {
+      const users = await User.all()
+      return response.status(200).send(users)
+    } catch (error) {
+      return response.status(400).json({ message: 'Users not exists' })
+    }
   }
 
   public async update({ request, response }: HttpContextContract) {
+    const { id } = request.params()
+
+    if (!Number(id)) return response.status(400).send({ message: 'ID Not number' })
+
     const data = request.body()
 
-    if (!Number(data.id)) return response.status(400).send({ message: 'ID Not number' })
-
     try {
-      const user = await User.findOrFail(Number(data.id))
+      const user = await User.findOrFail(id)
       await user.merge(data).save()
       return response.status(200).send({ message: 'Data updated with success' })
     } catch (error) {
